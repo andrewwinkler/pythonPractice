1a78f02292051ede182a40a9084bf452dc85d6c1
Andrew Winkler <andrew.winkler@me.com>
2018-03-03 14:34:28 -0700

    Additions, fixes

--- writingIdomatic.py
+++ writingIdomatic.py
@@ -1,17 +1,26 @@
 # /pythonPractice/writingIdomatic.py
+# -*- coding: UTF-8 -*-
 
 from __future__ import print_function
 
-print("\n--Let's Practice!\n--")
+print("\n--Let's Practice!--\n")
 
 # Notes:    When using if statements do the error checking FIRST, then have the code that supposed to run normally, 
 #           This lets a reader easily skip to what should happen.
 
+
 # Notes:    Chain arguments in the if statements if can
+
 
 # Notes:    Don’t repeat a '==' comparison in an if statement, use a list or set when you can. ‘x’ in ‘some_list’ can return true or false
 # Ex:
 
+is_generic_name = False
+name = 'Tom'
+if name == 'Tom' or name == 'Dick' or name == 'Harry':
+    is_generic_name = True
+print(is_generic_name) # prints 'True'
+
 name = 'Tom'
 is_generic_name = name in ('Tom', 'Dick', 'Harry')
-print(is_generic_name) # prints 'True'
+print(is_generic_name) # also prints 'True'
